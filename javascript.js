f48ae2a2083faf01c2c62e1db170855dd14189b5
Lilith Green <Noelia Scriorra>
2022-10-11 22:41:55 -0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,13 @@
+for (var adj of [
+    'Raging',
+    'Happy',
+    'Hungry'
+  ]) {
+    for (var noun of [
+      'Pharmacists',
+      'Squids',
+      'Twins'
+    ]) {
+      console.log('The ' + adj + ' ' + noun);
+    }
+  }
